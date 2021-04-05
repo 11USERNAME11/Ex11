1552543e22d3e770e45c407f3987026bbcb9a93f
NoName <ihackit595@gmail.com>
2021-04-05 18:28:03 +0300

    wrote demonstration

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,19 @@ int main() {
     Cage* f = new Cage();
     Cage* s = new Cage();
     Cage* t = new Cage();
+    Cage* fo = new Cage();
+    Cage* fi = new Cage();
+    Cage* si = new Cage();
+    Cage* se = new Cage();
     f->on();
-    // t->on();
+    fi->on();
     Train tr = Train(f, s);
-    // tr.printCagesState();
-    // tr.addCage(t, true);
-    // tr.printCagesState();
+    tr.addCage(t, true);
+    tr.addCage(fo, false);
+    tr.addCage(fi, true);
+    tr.addCage(si, false);
+    tr.addCage(se, true);
+    tr.printCagesState();
     std::cout << tr.findLength() << std::endl;
     return 0;
 }
